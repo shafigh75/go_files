ba83d0ae17d58c036918dbf78a25a49882a0ff80
mohammad shafighi <59147756+shafigh75@users.noreply.github.com>
2025-02-19 09:44:10 +0330

    Update main.py

--- hugging_face/main.py
+++ hugging_face/main.py
@@ -27,3 +27,23 @@ response = requests.post(
 )
 
 print(response.text)
+
+
+# sample image generation: 
+
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+headers = {"Authorization": "Bearer hf_***"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": "Astronaut riding a horse",
+})
+
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
